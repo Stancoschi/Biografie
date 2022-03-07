a6a98235f962360a37abf9fed7cc810bd1ac844d
Stanco <80153045+Stancoschi@users.noreply.github.com>
2022-03-07 21:18:30 +0200

    Add files via upload

---
+++ Biografie Stancoschi Eduard-Robert/main.js
@@ -0,0 +1,7 @@
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+
